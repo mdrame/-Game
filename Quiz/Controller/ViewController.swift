7e2468b045b3c6319967d8567c6b9020bf57232b
mohammed <mdrame113@gmail.com>
2019-11-23 21:36:14 -0800

    Front End 80% Done

--- Quiz/Controller/ViewController.swift
+++ Quiz/Controller/ViewController.swift
@@ -10,30 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // variable
+    var scoreCount : Int?
+    
     // IBOutlets
-    @IBOutlet weak var sidBarOutlet: UIButton!
-    @IBOutlet var colorbuttonOutlet: [UIButton]!
-    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var sideBarCollection: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet var true_false_outlet: [UIButton]!
+    @IBOutlet weak var scoreLabelOutlet: UILabel!
     
     
-    
-    
-    // Variables
+    var dict: [Int: UIColor] = [0: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), 1: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)]
+    // colors in lower case so i can use for both uicolor names .text property
+    var randomColor = ["red", "yellow", "blue", "black", "orange", "skyblue", "pink", "green", "purple", "white"]
 
+    let picked = Int.random(in: 0...9)
+    let randomBg = Int.random(in: 0...1)
+    
     
     // UIFunctions ( In-order to not occupy viewDidLoad func )
     
     func uiButtonsDesigned() {
         
-        sidBarOutlet.layer.borderWidth = 0.0
+        // sidde button designs
+        sideBarCollection.layer.borderWidth = 3.0
+        sideBarCollection.layer.borderColor = #colorLiteral(red: 1, green: 0.2256877422, blue: 0.0854042545, alpha: 1)
+        sideBarCollection.layer.cornerRadius = 10.0
+        
         
     
+    }
+    
+    func assigningTittle() {
+        
+        scoreLabelOutlet.text = "\(scoreCount ?? 100)"
+        resultLabel.text = randomColor[picked]
+        resultLabel.textColor = dict[randomBg]
+        
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        uiButtonsDesigned()
+        assigningTittle()
+       
+        
     }
     
     
@@ -49,6 +73,22 @@ class ViewController: UIViewController {
         
     }
     
+    
+    
+    @IBAction func action(_ sender: UIButton) {
+        
+        
+        
+      
+        
+        if sender.tag == 0 {
+            
+        } else if sender.tag == 1 {
+            
+        }
+        
+        
+    }
     
     
     
